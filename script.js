28b287a03144fb73e186357d6da25ab46567e898
Deepanshu9643 <deepanshukhorwal7@gmail.com>
2024-09-11 18:05:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+const progress = document.querySelector('#progress');
+const prev = document.querySelector('#prev');
+const next = document.querySelector('#next');
+const circles = document.querySelectorAll('.circle');
+
+
+next.addEventListener("click",mota)
+function mota(){
+	progress.style.backgroundColor ="#3498db";
+	progress.style.width = "100px";
+	progress.style.height = "10px";
+}
